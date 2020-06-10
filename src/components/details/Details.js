7c35ba26786324c7c1d9ba8aba24b9449ca51ce2
Lauren Xie <lrnxie@gmail.com>
2020-06-10 12:24:35 -0400

    Display movie/tv details

--- src/components/details/Details.js
+++ src/components/details/Details.js
@@ -1,7 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import MovieDetails from "./MovieDetails";
+import TvDetails from "./TvDetails";
+
+import { makeStyles } from "@material-ui/core/styles";
+import { CircularProgress } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  loading: {
+    display: "flex",
+    justifyContent: "center",
+    marginTop: theme.spacing(8),
+  },
+}));
 
 const Details = ({ match }) => {
+  const classes = useStyles();
+  const [details, setDetails] = useState({});
+  const [loading, setLoading] = useState(true);
   const { type, id } = match.params;
 
   useEffect(() => {
@@ -9,17 +25,20 @@ const Details = ({ match }) => {
       const res = await axios.get(
         `https://api.themoviedb.org/3/${type}/${id}?api_key=${process.env.REACT_APP_TMDB_API_KEY}`
       );
-      console.log(res.data);
+      setDetails(res.data);
+      setLoading(false);
     };
     fetchData();
   }, [type, id]);
 
-  return (
-    <div>
-      <p>
-        This is the detail page for {type} {id}
-      </p>
+  return loading ? (
+    <div className={classes.loading}>
+      <CircularProgress />
     </div>
+  ) : type === "movie" ? (
+    <MovieDetails details={details} />
+  ) : (
+    <TvDetails details={details} />
   );
 };
 
---
+++ src/components/details/TvDetails.js
@@ -0,0 +1,92 @@
+import React from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Rating from "@material-ui/lab/Rating";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    margin: theme.spacing(2),
+    [theme.breakpoints.down("sm")]: {
+      flexDirection: "column",
+    },
+  },
+  rating: {
+    display: "flex",
+    alignItems: "center",
+    "& > p": {
+      marginLeft: theme.spacing(1),
+    },
+  },
+  poster: {
+    height: "325px",
+    objectFit: "scale-down",
+    [theme.breakpoints.down("sm")]: {
+      marginBottom: theme.spacing(2),
+    },
+  },
+  details: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(2),
+    },
+  },
+}));
+
+const MovieDetails = ({ details }) => {
+  const classes = useStyles();
+  const {
+    poster_path,
+    name,
+    first_air_date,
+    number_of_seasons,
+    genres,
+    vote_average,
+    overview,
+  } = details;
+
+  return (
+    <div className={classes.root}>
+      <img
+        className={classes.poster}
+        src={
+          poster_path
+            ? `https://image.tmdb.org/t/p/w500/${poster_path}`
+            : "https://www.themoviedb.org/assets/2/v4/glyphicons/basic/glyphicons-basic-38-picture-4ee37443c461fff5bc221b43ae018a5dae317469c8e2479a87d562537dd45fdc.svg"
+        }
+        alt={name}
+      />
+
+      <div className={classes.details}>
+        <Typography variant="h5">{name}</Typography>
+        <Typography variant="subtitle2">
+          {first_air_date} | {number_of_seasons}{" "}
+          {number_of_seasons === 1 ? "season" : "seasons"}
+        </Typography>
+        <Typography variant="subtitle2">
+          Genres:{" "}
+          {genres &&
+            genres.map((genre, index) => (index ? ", " : "") + genre.name)}
+        </Typography>
+
+        <div className={classes.rating}>
+          <Rating value={vote_average / 2} precision={0.5} readOnly />
+          <Typography variant="body2">
+            {vote_average === 0 ? "N/A" : vote_average}
+          </Typography>
+        </div>
+
+        <div>
+          <Typography variant="h6">Overview</Typography>
+          <Typography variant="body2">{overview}</Typography>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieDetails;
